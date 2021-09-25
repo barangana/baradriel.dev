b158482ae7c719b07e146d6c0d0ab13204724ceb
Adriel <baranganadriel@gmail.com>
2021-09-24 22:49:52 -0400

    chore: update config files with icons, clean boilerplate

---
+++ utils/info.js
@@ -0,0 +1,11 @@
+const infos = {
+  title: "Adriel Barangan",
+  header:
+    "Software and hardware enthusiast. I love building things for the web.",
+  who: "I'm a full-stack developer based in Montreal. I mainly work with JavaScript but always open to learning more.",
+  about_me:
+    "Hello! My name is Adriel, I am a full stack developer and I enjoy creating and learning new things. It all started when I was a child and I got hooked on to video games and ended up wanting to learn more on how they were made.",
+  what: "Full-Stack Developer",
+};
+
+export default infos;
---
+++ utils/skills.js
@@ -0,0 +1,41 @@
+const knowledge = [
+  {
+    id: 1,
+    title: "Languages",
+    skills: [
+      { name: "Java" },
+      { name: "JavScript" },
+      { name: "TypeScript" },
+      { name: "PHP" },
+      { name: "C#" },
+      { name: "CSS/Sass" },
+      { name: "SQL" },
+      { name: "Perl" },
+      { name: "GraphQL" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Frameworks",
+    skills: [
+      { name: "React.js" },
+      { name: "Express.js" },
+      { name: "Next.js" },
+      { name: "Jest" },
+      { name: "Tailwind CSS" },
+      { name: "Node.js" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Tools",
+    skills: [
+      { name: "Git & Github" },
+      { name: "Postman" },
+      { name: "MongoDB" },
+      { name: "Subversion" },
+    ],
+  },
+];
+
+export default skills;
