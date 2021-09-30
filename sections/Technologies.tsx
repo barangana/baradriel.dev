4c7403d1c1370912d8b696c0447028762e1533a3
Adriel <baranganadriel@gmail.com>
2021-09-29 23:31:13 -0400

    chore: added functioncomponent types and changed all js to ts.

--- sections/Technologies.tsx
+++ sections/Technologies.tsx
@@ -1,10 +1,21 @@
 import { FunctionComponent } from "react";
-import skills from "../utils/skills";
+
+import knowledges from "../utils/skills";
+import Technology from "../components/Technologies/Technology";
+
+export type KnowledgeType = {
+  id: number;
+  title: string;
+  skills: [{ name: string }];
+};
 
 const Technologies: FunctionComponent = () => {
   return (
     <section>
       <h1>Technologies Section</h1>
+      {knowledges.map((knowledge) => {
+        return <Technology key={knowledge.id} knowledge={knowledge} />;
+      })}
     </section>
   );
 };
---
+++ utils/projects.ts
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: 1,
+    title: "CipherPact",
+    description:
+      "Suite of financial agreements on Cardano, Ergo such as escrows with collateral, zero-coupon bonds and contract for differences into smart contracts (with a web UI) for a trust-less, decentralized and publicly verifiable agreement. ",
+    tech: ["Next.js", "React.js", "Ergo", "Blockfrost", "Styled-Components"],
+  },
+  {
+    id: 2,
+    title: "Anime Database Search",
+    description:
+      "Utilizes the Jikan API and makes GET requests to the endpoint. Returns data and displays general information about the show.",
+    tech: ["React.js", "Jikan API"],
+  },
+];
+
+export default projects;
