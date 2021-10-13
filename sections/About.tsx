cd6b3ed2d2b225f92d0234e1ac80981a38625cc4
Adriel <baranganadriel@gmail.com>
2021-10-12 23:27:00 -0400

    feat: about structuring done, styling needed

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,35 +1,33 @@
-import { FunctionComponent } from "react";
+import { FC } from "react";
 import info from "../utils/info";
+import Image from "next/image";
 
 // Styles
-import {
-  Heading,
-  Box,
-  Text,
-  Image,
-  SimpleGrid,
-  ListItem,
-} from "@chakra-ui/react";
+import { Heading, Box, Text, Flex } from "@chakra-ui/react";
 
-const About: FunctionComponent = () => {
+const About: FC = () => {
   return (
     <Box height="100vh" id="about">
-      <SimpleGrid columns={2} position="relative" top="10%" padding="5em">
-        <Box bg="teal.400" p="5em">
-          <Heading p={5}>About me</Heading>
-          <Box position="relative" top="5%">
-            <Text p={5}>{info.about}</Text>
-            <Text p={5}>{info.about_two}</Text>
-            <Text p={5}>{info.about_three}</Text>
-            {info.about_tech.map((tech) => {
-              return <li key={tech.length + 1}>{tech}</li>;
-            })}
-          </Box>
+      <Box position="absolute" left="20%">
+        <Box position="relative" top="48" width="100%">
+          <Heading pb="8">About me</Heading>
+          <Flex>
+            <Box pl="8" pr="4" width="50%">
+              <Text pb="2">{info.about}</Text>
+              <Text pb="2">{info.about_two}</Text>
+              <Text pb="2">{info.about_three}</Text>
+              <ul>
+                {info.about_tech.map((tech) => {
+                  return <li key={tech.length + 1}>{tech}</li>;
+                })}
+              </ul>
+            </Box>
+            <Box>
+              <Image src="/selfie.jpg" alt="selfie" width={300} height={300} />
+            </Box>
+          </Flex>
         </Box>
-        <Box bg="teal.500" p="5em">
-          <Image></Image>
-        </Box>
-      </SimpleGrid>
+      </Box>
     </Box>
   );
 };
