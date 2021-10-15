6b750f11fd155d07e05593e965a56bc94b4b0f88
Adriel <baranganadriel@gmail.com>
2021-10-14 22:08:43 -0400

    feat: added head tag with title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
+import Head from "next/head";
+import info from "../utils/info";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>{info.title}</title>
+        <meta name="description" content={info.who_description} />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 export default MyApp;
