284a2527156f935519b99717cd3b5db6d4b314a7
Adriel <baranganadriel@gmail.com>
2021-11-01 21:20:36 -0400

    fix: fixed responsivity on mobile devices for nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,13 +5,13 @@ import NavButton from "./NavButton";
 import info from "../utils/info";
 
 // Styles
-import { Button, Box, HStack } from "@chakra-ui/react";
+import { Button, Box, HStack, Flex, Text } from "@chakra-ui/react";
 
 const buttons = [
-  { label: "About", link: "about" },
-  { label: "Technologies", link: "technologies" },
-  { label: "Projects", link: "projects" },
-  { label: "Contact", link: "contact" },
+  { label: "About", link: "#about" },
+  { label: "Technologies", link: "#technologies" },
+  { label: "Projects", link: "#projects" },
+  { label: "Contact", link: "#contact" },
 ];
 
 const resumeHandler = (e: any): void => {
@@ -21,28 +21,22 @@ const resumeHandler = (e: any): void => {
 
 const Header: FC = () => {
   return (
-    <Box position="absolute" pt="6" w={["95%", "75%", "100%"]} as="header">
-      <Box float="left" pl="1.75em">
-        Adriel Barangan
-      </Box>
-      <Box position="relative" float="right">
-        <HStack>
-          <NavButton buttons={buttons} />
-          <Box pr={["0", "4"]}>
-            <Button
-              onClick={resumeHandler}
-              size="md"
-              variant="outline"
-              color="brand.600"
-              bgColor="brand.200"
-              border="2px"
-            >
-              Resume
-            </Button>
-          </Box>
-        </HStack>
-      </Box>
-    </Box>
+    <Flex justify="space-between" p="5" direction={["column", "row"]}>
+      <Text>Adriel Barangan</Text>
+      <HStack>
+        <NavButton buttons={buttons} />
+        <Button
+          onClick={resumeHandler}
+          size="md"
+          variant="outline"
+          color="brand.600"
+          bgColor="brand.200"
+          border="2px"
+        >
+          Resume
+        </Button>
+      </HStack>
+    </Flex>
   );
 };
 
