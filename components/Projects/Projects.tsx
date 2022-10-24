219cc9fc0de04aca957af4aeabfedba0a207acd0
Adriel <baranganadriel@gmail.com>
2022-10-24 19:58:39 -0400

    fix: fixed design on tablet

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -7,7 +7,12 @@ export const Projects = () => {
   return (
     <Box mt='12' mb='12' id='projects'>
       <Heading>{info.projects_title}</Heading>
-      <SimpleGrid columns={[1, 2, 3]} spacing={['6', '24']} mt='16' mb='16'>
+      <SimpleGrid
+        columns={[1, 2, 2, 3]}
+        spacing={['6', '6', '8', '10', '24']}
+        mt='16'
+        mb='16'
+      >
         {projects.map((project) => (
           <Cards key={project.id} project={project} />
         ))}
