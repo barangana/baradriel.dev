41d50c91cd601d23f9817060b39fb4de07235eaa
barangana <baranganadriel@gmail.com>
2024-11-16 14:48:10 -0500

    version 2 initial commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-  },
-}
--- theme/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './button'
-export * from './divider'
-export * from './link'
